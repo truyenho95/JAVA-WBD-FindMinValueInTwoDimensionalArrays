7ab0bb0dff65895a1ebb952379599fa1cde5a2bc
truyenho95 <truyenho.webdev@gmail.com>
2018-12-08 05:23:08 +0700

    Change user input feature for Min Value Two Dimensional Array class

--- src/MinValueInTwoDimensionalArray.java
+++ src/MinValueInTwoDimensionalArray.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class MinValueInTwoDimensionalArray {
   public static void main(String[] args) {
-    int[][] twoDimensionalArrays = {
-      {1,3,5},
-      {9,6,16},
-      {92,-10,6}
-    };
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Độ rộng của ma trận là:");
+    int height = scanner.nextInt();
+    System.out.println("Độ dài của ma trận là:");
+    int width = scanner.nextInt();
+    int[][] twoDimensionalArrays = new int[height][width];
+
+    System.out.println("Nhập mảng:");
+    for (int i = 0; i < twoDimensionalArrays.length; i++)
+      for (int j = 0; j < twoDimensionalArrays[0].length; j++) {
+        System.out.print("Nhập giá trị phần tử có chỉ số [" + i + "][" + j + "]");
+        twoDimensionalArrays[i][j] = scanner.nextInt();
+      }
+
+    System.out.println("Mảng vừa nhập là:");
+    for (int i = 0; i < twoDimensionalArrays.length; i++) {
+      for (int j = 0; j < twoDimensionalArrays[0].length; j++) {
+        System.out.print(twoDimensionalArrays[i][j]+"\t");
+      }
+      System.out.print("\n");
+    }
 
     int minValueInTwoDimensionalArray = findMinValueInTwoDimensionalArrays(twoDimensionalArrays);
     System.out.print("Giá trị nhỏ nhất trong mảng là: ");
@@ -13,8 +31,8 @@ public class MinValueInTwoDimensionalArray {
 
   public static int findMinValueInTwoDimensionalArrays(int[][] arr) {
     int min = arr[0][0];
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
         if (min > arr[i][j]) {
           min = arr[i][j];
         }
